111f1a156b1758e599a38404bfa559c014e506ed
Emaine <ainebyoonaemmanuels@gmail.com>
2017-07-06 09:41:22 +0300

    added bootstrap and changed the database to wms in opendbo.php fie, I also adde the register.php for registration of users

---
+++ register.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	 <title>register</title>
+	 <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<style>
+		 .heading{
+			align:center;
+			font-family:papyrus;
+			font-size:50px;
+		 }
+		
+		</style>
+	</head>
+	<body>
+	 
+	</body>
+</html>
