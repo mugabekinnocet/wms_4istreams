602eb1163e6dcf931158b93ba29645ffe54736b4
Emaine <ainebyoonaemmanuels@gmail.com>
2017-07-14 13:44:24 +0300

    the login form

---
+++ loginform.inc.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+		  Log In
+		</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<style>
+		 .heading{
+			align:center;
+			font-family:papyrus;
+			font-size:50px;
+		 }
+		
+		</style>
+	</head>
+	<body>
+	<div class="header">
+<div class="navbar-inner">
+<div class="container">
+<a class="btn btn-navbar" data-toggle="collapse"
+data-target=".nav-collapse">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</a>
+<!-- Leave the brand out if you want it to be shown when other elements
+are collapsed... -->
+<a href="#" class="brand">Project Name</a>
+<!-- Everything that you want collapsed, should be added to the collapse
+div. -->
+<div class="nav-collapse collapse">
+<!-- .nav, .navbar-search etc... -->
+</div>
+</div>
+</div>
+</div>
+	
+		   <h1 class = "heading">Hello!!!!!</h1>
+
+		    <div><fieldset class="fieldset" >
+			 <legend style = "align:center;">Login here</legend>
+				<div><form method="POST" action = "">
+					<div><table style="margin:auto;" class="table table ">
+					  <tr>
+						 <td><label>Name</label></td>
+						 <td><input type="text" placeholder="enter user name" required class="form-control"><td>
+					  </tr>
+					  <tr>
+						 <td><label>Password</label></td>
+						 <td><input type="password" required class="form-control"><td>
+					  </tr>&nbsp;
+					
+    
+					  <tr>
+					  <td></td><td><input type = "submit" class = "btn btn-primary ">
+					<input type="checkbox"><label for="checkbox">Keep me logged in</label>
+					<input type="Reset" value="Reset" name="Reset"></td>
+						 <td><a href = "register.php">create account</a></td>
+						 <td><a href = "administrator.php">admin</a></td>
+						 <td><a href = "finance.php">finance</a></td>
+						 <td><a href = "receipt.php">receipt</a></td>
+						 <td><a href = "retrieve.php">retrieve</a></td>
+						 <td><a href = "submitregister.php">submit</a></td>
+						 <td><a href = "technician.php">technician</a></td>
+					  </tr>
+					  
+					 <table></div>
+				</form></div>
+	        </fieldset></div>
+	</body>
+</html>
