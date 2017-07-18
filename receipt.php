30740bbe73a73a14b4788c4b63be97df7bbed945
mugabe innocent <mugabe innocent>
2017-07-18 05:20:54 -0700

    login

--- receipt.php
+++ receipt.php
@@ -1,3 +1,4 @@
+<?php
 <html>
 <style>
 #page {
@@ -33,25 +34,53 @@
 						<form id="test">
                <div style="width:500px;height:400px;background-color:white;">
                   <center><img src="istrm.png" width="100" height="50"></center>
-					
-	<div>Date...............................</div>
-	<div>ServiceId..........................</div>
-	<div>Customer's  name .......................................................................</div
-	<div>Item(s)................................................................................<br>
-		..................................................................................<br>
-	......................................<br>Brought On<input type="date"> Picked On<input  type="date"</div>
-	<div>Problem(s)................................................................................<br>
-		.............................................................................................<br>
-		..............................................................................................<br>
-		..................................................................................................</div>
-		<div>Amount Agreed in words.......................................................................<br>
-			.................................................................................................<br>
-			Amount Paid<input id="money"><center>Balance <input></center></div> 
-		<div>Received by..............................................Approved by..........................................</div>
-		
-		<div>Thank you for your coorperation</div>
-			
-		
+	<?php				
+	require("opendbo.php");
+
+$result = mysql_query("SELECT * FROM user");
+
+echo "<table style='border-style:groove; border-width:5px; color:#960; border-collapse:collapse; width:65%;margin-left:5.5%;background-color: white;'>
+<tr style='background-color: green; color:black;'>
+<th style='border-style:groove; border-width:2px;'>SERVICEID</th>
+<th style='border-style:groove; border-width:2px;'FIRST NAME</th>
+<th style='border-style:groove; border-width:2px;'LAST NAME</th>
+<th style='border-style:groove; border-width:2px;'>ITEMS</th>
+<th style='border-style:groove; border-width:2px;'>SERVICE-DATE</th>
+<th style='border-style:groove; border-width:2px;'>AGREED-AMOUNT</th>
+<th style='border-style:groove; border-width:2px;'>PAID</th>
+<th style='border-style:groove; border-width:2px;'>PHONE NUMBER</th>
+<th style='border-style:groove; border-width:2px;'>UPDATE PAID</th>
+<th style='border-style:groove; border-width:2px;'>EDIT</th>
+<th style='border-style:groove; border-width:2px;'>DELETE</th>
+
+</tr>";
+
+while($row = mysql_fetch_array($result))
+  {
+echo "
+  <tr>
+  <td  style='border-style:groove; border-width:2px;' >" . $row['serviceId'] . "</td>
+   <td style='border-style:groove; border-width:2px;'>" . $row['firstname'] . "</td>
+   <td style='border-style:groove; border-width:2px;'>" . $row['lastname'] . "</td>
+   <td style='border-style:groove; border-width:2px;'>" . $row['items'] . "</td>
+   <td style='border-style:groove; border-width:2px;'>" . $row['servicedate'] . "</td>
+   <td style='border-style:groove; border-width:2px;'>" . $row['agreedamount'] . "</td>
+   <td style='border-style:groove; border-width:2px;'>" . $row['paid'] . "</td>
+    <td style='border-style:groove; border-width:2px;'>" . $row['phone'] . "</td>
+   
+   
+  <td style='border-style:groove; border-width:2px;'>
+   <input type='hidden' name='id' value='".$row['serviceId']."'> 
+   <input type='submit' value='Update paid'>
+   </td>
+   </form>
+   <form action='editingform2.php' method='post'>
+  <td style='border-style:groove; border-width:2px;'>
+   <input type='hidden' name='id' value='".$row['serviceId']."'> 
+   <input type='submit' value='edit'>
+   </td>
+   ";
+   ?>
                 </div>
             </div>
 			</fieldset>
@@ -67,4 +96,5 @@
 </body>
 
 </html>
+?>
 
